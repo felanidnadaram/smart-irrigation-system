75c4fae1f4869991e12af1262230716849ea9b0e
felanidnadaram <amnn60985@gmail.com>
2026-07-10 14:28:27 +0330

    modified config.py to support db isolation tets

--- app/config.py
+++ app/config.py
@@ -3,6 +3,10 @@ import os
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "precision_agriculture")
 
+TESTING = os.getenv("TESTING", "0") == "1"
+if TESTING:
+    DATABASE_NAME = os.getenv("TEST_DATABASE_NAME", "precision_agriculture_test")
+
 SECRET_KEY = os.getenv("SECRET_KEY", "a3f5b8c1d4e7f0a2b6c9d3e8f1a4b7c0d5e9f2a6")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
